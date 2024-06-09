9f1f6a94d71a74998b45819dcd568970ff7b93f1
dat <tranminhdat0805@gmail.com>
2024-06-09 16:35:59 +0700

    Diagnose feature doing

---
+++ src/models/Diagnose.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+const schema = new mongoose.Schema(
+    {
+        disease: {
+            type: String,
+            default: "",
+            required: true,
+        },
+        description: {
+            type: String,
+            default: "",
+        },
+        symptoms: {
+            type: Array,
+            default: [],
+        },
+        treatment: {
+            type: String,
+            default: "",
+        },
+        otherDiseases: {
+            type: Array,
+            default: [],
+        },
+        status: {
+            enum: ["active", "inactive"],
+            type: String,
+            default: "active",
+        },
+        createdDate: {
+            type: Date,
+            default: Date.now,
+        },
+    }
+);
+
+export const Diagnose = mongoose.model('Diagnose', schema);
